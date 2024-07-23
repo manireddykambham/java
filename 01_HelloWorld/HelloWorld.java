d971486c0a0b95395787a0d33bf5dc6bd16a90bd
Mani Shankar <manishankar377@gmail.com>
2024-07-23 00:47:04 -0500

    two types of comments

--- 01_HelloWorld/HelloWorld.java
+++ 01_HelloWorld/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");//hello world
+        System.out.println("Hello World");
+        //hello world
+        /*This is multi line
+        so I'm using this type of comments
+         */
     }
 }
