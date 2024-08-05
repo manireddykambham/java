11b14d7b4fbfd12e154fde2680be7e886e8391c9
Mani Shankar <manishankar377@gmail.com>
2024-08-05 00:56:05 -0500

    Add new variables

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -1,6 +1,10 @@
+package HelloWorld;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
         //hello world
         /*This is multi line
         so I'm using this type of comments
