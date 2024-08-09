51e8220ea77c35b3d5b0bf82ccd9889c687b31ec
Mani Shankar <manishankar377@gmail.com>
2024-08-09 16:26:42 -0500

    add number print

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -5,6 +5,9 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("I am learning Java.");
         System.out.println("It is awesome!");
+        System.out.println(3);
+        System.out.println(358);
+        System.out.println(50000);
         //hello world
         /*This is multi line
         so I'm using this type of comments
