4badc7ccf6f117d519cb048a82bb5f488b2125f6
Mani Shankar <manishankar377@gmail.com>
2024-09-09 22:05:26 -0500

    add changes

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
-
+import HelloWorld.HelloWorld;
 
 public class Main {
     public static void main(String[] args) {
         //System.out.println("\nHello World\n");
-
+        HelloWorld helloWorld = new HelloWorld();
+        System.out.println(helloWorld);
 
     }
 }
