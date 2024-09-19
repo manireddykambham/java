687844259dcd212025cf0b09042cdb93f7183631
Mani Shankar <manishankar377@gmail.com>
2024-09-19 15:34:46 -0500

    more methods train

--- Main.java
+++ Main.java
@@ -1,17 +1,25 @@
 import HelloWorld.HelloWorld;
 import methods.ObjectClass;
+import methods.PublicVsStatic;
+import methods.SecondClass;
 
 import static methods.MethodBasic.myMethod;
+import static methods.PublicVsStatic.myStaticMethod;
 import static variables.AllVariables.variables;
 
 public class Main {
     public static void main(String[] args) {
         //System.out.println("\nHello World\n");
-        ObjectClass objectClass = new ObjectClass();
-        System.out.println(objectClass.x);
+        SecondClass.main(new String[0]);
         System.out.println("helloWorld");
         variables();
         myMethod();
+        PublicVsStatic publicVsStatic = new PublicVsStatic();
+        publicVsStatic.myPublicMethod();
+        myStaticMethod();
+        publicVsStatic.fullThrottle();
+        publicVsStatic.speed(900);
+
 
     }
 }
--- methods/PublicVsStatic.java
+++ methods/PublicVsStatic.java
@@ -2,14 +2,26 @@ package methods;
 
 public class PublicVsStatic {
     // Static method
-    static void myStaticMethod() {
+    public static void myStaticMethod() {
         System.out.println("Static methods can be called without creating objects");
+        int x=5;
     }
 
     // Public method
     public void myPublicMethod() {
         System.out.println("Public methods must be called by creating objects");
     }
+    // Create a fullThrottle() method
+    public void fullThrottle() {
+        System.out.println("The car is going as fast as it can!");
+    }
+
+    // Create a speed() method and add a parameter
+    public void speed(int maxSpeed) {
+        System.out.println("Max speed is: " + maxSpeed);
+    }
+
+    // Inside main, call the methods on the myCar object
 
     // Main method
     public static void main(String[] args) {
@@ -18,5 +30,9 @@ public class PublicVsStatic {
 
         PublicVsStatic myObj = new PublicVsStatic(); // Create an object of Main
         myObj.myPublicMethod(); // Call the public method on the object
+
+        PublicVsStatic myCar = new PublicVsStatic();   // Create a myCar object
+        myCar.fullThrottle();      // Call the fullThrottle() method
+        myCar.speed(200);
     }
 }
